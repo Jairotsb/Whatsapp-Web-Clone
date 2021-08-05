b3f322e5df4245a933be90649f6d16ea59bb0dd7
Jairo-Arcy <jairo_tunisse@outlook.com>
2021-08-04 23:33:11 -0300

    Update Project to material-ui v4.12.3 :heavy_check_mark:

--- src/App.js
+++ src/App.js
@@ -1,122 +1,165 @@
 import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
+//import clsx from 'clsx'
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SendIcon from '@material-ui/icons/Send';
 import ImageIcon from "@material-ui/icons/Image";
 import WorkIcon from "@material-ui/icons/Work";
 import BeachAccessIcon from "@material-ui/icons/BeachAccess";
-import { Paper, Typography, Grid, Card, CardHeader, CardContent, Avatar, List, ListItem, ListItemText, IconButton, Button } from "@material-ui/core";
-const styles = () => ({
- root: {
-  padding: "50px 100px",
-  zIndex: 999,
-  position:"absolute",
- },
- card: {
-  display:"flex",
-  height: "calc(100vh - 100px)"
- },
- rightBorder: {
-  borderRight: "solid #d0D0D0 1px"
- },
- content: {
-  marginTop: 0
- },
- background: {
-  position: "absolute",
-  height: 200,
-  width: "100%",
-  top: 0,
-  background: "#7159C1"
- },
- rightContainer:{
-  background: "url(https://hdwallsource.com/img/2014/8/my-neighbor-totoro-wallpaper-27981-28703-hd-wallpapers.jpg) center center",
-  backgroundPosition: "center center",
-  backgroundRepeat: "no-repeat", 
-  backgroundSize: "cover",
-  flex:1
- },
- heightAdjust: {
-  display:"flex",
-  flexDirection: "column"
- },
- paper:{
-  background: "#9de1fe",
-  padding: 20
- },
+import InputBase from '@material-ui/core/InputBase';
+import { Paper, Typography, Grid, Card, CardHeader, CardContent, Avatar, List, ListItem, ListItemText, IconButton } from "@material-ui/core";
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: "50px 100px",
+    zIndex: 999,
+    position: "absolute",
+  },
+  card: {
+    display: "flex",
+    height: "calc(100vh - 100px)"
+  },
+  rightBorder: {
+    borderRight: "solid #d0D0D0 1px"
+  },
+  content: {
+    marginTop: 0
+  },
+  background: {
+    position: "absolute",
+    height: 200,
+    width: "100%",
+    top: 0,
+    background: "#7159C1"
+  },
+  rightContainer: {
+    background: "url(https://hdwallsource.com/img/2014/8/my-neighbor-totoro-wallpaper-27981-28703-hd-wallpapers.jpg) center center",
+    backgroundPosition: "center center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    flex: 1
+  },
+  heightAdjust: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  paper: {
+    background: "#9de1fe",
+    padding: 20
+  },
   information: {
     color: "#444"
-  }, 
+  },
 
   avatar: {
     background: 'purple'
-  }
-});
+  },
 
-const App = ({classes}) => (
-  <div>
-    <div className={classes.background}/>
-    <Grid container className={classes.root}>
-      <Grid item xs={12}>
+  inputArea: {
+//    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background: '#dddbd1',
+    borderRadius: '0px',
+    height: '62px'
+    //width: '100%',
+  },
+
+  input: {
+    marginLeft: theme.spacing(1),
+//    flex: 1,
+    borderRadius: '10px',
+    background: 'white',
+    padding: '3px',
+    width: '85%',
+    height: '42px'
+  },
+
+}))
+
+export default function App() {
+
+  const classes = useStyles()
+
+  const list = [
+    { id: 1, name: "Diego", text: "Olá mundo", image: <ImageIcon /> },
+    { id: 2, name: "Jairo", text: "Boa tarde", image: <WorkIcon /> },
+    { id: 3, name: "Willian", text: "Boa noite", image: <WorkIcon /> },
+    { id: 4, name: "Jorge", text: "Material-ui its good", image: <BeachAccessIcon /> }
+  ]
+
+  let title = 'Diego'
+  const changeName = () => {
+    CardHeader.title = 'Jairo'
+  }
+
+  const LeftContainer = () => (
+    <Grid item xs={3}>
+      <CardHeader
+        className={classes.rightBorder}
+        avatar={
+          <Avatar aria-label="Recipe" clasName={classes.avatar}>J</Avatar>
+        }
+        title="Jairo Arcy" />
+      <Paper className={classes.paper} elevation={0}>
+        <Typography className={classes.information} variant="subheader">
+          Aproveite o WhatsApp!
+        </Typography>
+      </Paper>
+      <List>
+        {list.map(item => (
+          <ListItem>
+            <Avatar className={classes.avatar}>{item.image}</Avatar>
+            <ListItemText primary={item.name} secondary={item.text} />
+          </ListItem>
+        ))}
+      </List>
+    </Grid>
+  );
+  const RightContainer = () => (
+    <Grid className={classes.heightAdjust} item xs={9}>
+      <CardHeader avatar={
+        <Avatar aria-label="Recipe" className={classes.avatar}>
+          <ImageIcon onClick={changeName} />
+        </Avatar>
+      }
+        action={
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={title}
+      />
+      <CardContent className={[classes.rightContainer, classes.content]} />
+      <Paper component="form" className={classes.inputArea}>
+        <InputBase
+          align={"center"}
+          className={classes.input}
+          placeholder="Digite uma mensagem"
+          inputProps={{ 'aria-label': 'Digite uma mensagem' }}
+        />
+        <SendIcon className={classes.iconSend}/>
+      </Paper>
+    </Grid>
+  );
+
+
+  return (
+    <>
+      <div className={classes.background} />
+      <Grid container className={classes.root}>
+        <Grid item xs={12}>
           <Card className={classes.card}>
             <Grid container>
-              <LeftContainer classes={classes}/>
-               <RightContainer classes={classes}/>
+              <LeftContainer classes={classes} />
+              <RightContainer classes={classes} />
             </Grid>
           </Card>
-            
-      </Grid>
-    </Grid>
-  </div>
-);
-const list  =[
-  {id: 1, name: "Diego", text: "Olá mundo", image:  <ImageIcon/>},
-  {id: 2, name: "Jairo", text: "Boa tarde", image: <WorkIcon/>},
-  {id: 3, name: "Willian", text: "Boa noite", image: <WorkIcon/>},
-  {id: 4, name: "Jorge", text: "Material-ui its good", image: <BeachAccessIcon/>}
-]
-let title = 'Diego'
-const changeName = () => {
-  CardHeader.title = 'Jairo'
-}
 
-const LeftContainer = ({classes}) => (
-  <Grid item xs={3}>
-    <CardHeader 
-    className={classes.rightBorder} 
-    avatar={
-      <Avatar aria-label="Recipe" clasName={classes.avatar}>J</Avatar>
-    }
-    title="Jairo Arcy"/>
-    <Paper className={classes.paper} elevation={0}>
-      <Typography className={classes.information} variant="subheader">
-          Aproveite o WhatsApp!
-      </Typography>
-    </Paper>
-    <List>
-      {list.map(item => (
-          <ListItem>
-             <Avatar className={classes.avatar}>{item.image}</Avatar> 
-             <ListItemText primary={item.name} secondary={item.text}/>
-          </ListItem>
-      ))}
-    </List>
-  </Grid>
-);
-const RightContainer =  ({classes}) => (
-  <Grid className={classes.heightAdjust} item xs={9}>
-    <CardHeader avatar={
-      <Avatar aria-label="Recipe" className={classes.avatar}>
-        <ImageIcon onClick={changeName}/>
-      </Avatar>
-    }
-    action ={
-      <IconButton>
-        <MoreVertIcon/>
-      </IconButton>
-    }
-    title={title}
-    />
-    <CardContent className={[classes.rightContainer, classes.content]} />
-  </Grid>
-);
-export default withStyles(styles)(App);
+        </Grid>
+      </Grid>
+    </>
+  )
+}
